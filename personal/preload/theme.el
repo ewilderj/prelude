84b3e21cd73ed36d178262f4dcb1440ee24f2087
Edd Wilder-James <ewilderj@github.com>
2025-03-05 08:50:55 -0800

    include rose pine theme as option

--- personal/preload/theme.el
+++ personal/preload/theme.el
@@ -1,3 +1,12 @@
 ;; (setq prelude-theme 'sanityinc-tomorrow-eighties)
 ;; (setq prelude-theme 'solarized-dark)
 (setq prelude-theme 'dracula)
+
+(use-package autothemer)
+(use-package dracula-theme)
+(use-package rose-pine-theme
+  :vc (:url "https://github.com/konrad1977/pinerose-emacs"
+            :rev :newest
+            :branch "main"))
+
+;; (setq prelude-theme 'rose-pine)
