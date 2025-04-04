f362c625d6f45be9d2de392c3115d3a26d537278
Edd Wilder-James <ewilderj@github.com>
2025-04-03 23:21:01 -0700

    semi sane

--- personal/gtd.el
+++ personal/gtd.el
@@ -8,6 +8,8 @@
   :custom
     (org-gtd-directory "~/org/gtd")
     (org-edna-use-inheritance t)
+    (org-gtd-clarify-show-horizons t)
+    (org-gtd-areas-of-focus '("Manager" "Copilot" "SFI" "Career" "EPD Ops"))
   :config
   (org-edna-mode)
   ;; Add custom keybindings for GTD functions
@@ -19,3 +21,18 @@
    ("C-c d s" . org-gtd-show-stuck-projects)
    :map org-gtd-process-map
    ("C-c c" . org-gtd-choose)))
+
+(defvar my-gtd-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "c" 'org-gtd-capture)
+    (define-key map "e" 'org-gtd-engage)
+    (define-key map "p" 'org-gtd-process-inbox)
+    (define-key map "n" 'org-gtd-show-all-next)
+    (define-key map "s" 'org-gtd-show-stuck-projects)
+    map)
+
+  "My GTD keymap.")
+
+(keymap-global-set "s-g" my-gtd-map)
+(define-key org-gtd-clarify-map (kbd "C-c c") #'org-gtd-organize)
+(define-key org-gtd-process-map (kbd "C-c c") #'org-gtd-choose)
