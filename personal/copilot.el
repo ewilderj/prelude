9c19f3a2561662c450fa154d22fb5f586d42461f
Edd Wilder-James <edd@me.com>
2025-04-05 20:17:54 -0700

    handle copilot initialization case more nicely

--- personal/copilot.el
+++ personal/copilot.el
@@ -1,11 +1,23 @@
 (use-package copilot
   :ensure t
-  :vc (:url "https://github.com/copilot-emacs/copilot.el"
-            :rev :newest
-            :branch "main"))
-
-(add-hook 'prog-mode-hook 'copilot-mode)
-
+  :demand t
+  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))
+  :config
+  (condition-case err
+      (progn
+        (message "Attempting to enable copilot-mode")
+        (copilot-mode 1)
+        (add-hook 'prog-mode-hook 'copilot-mode)
+        )
+    (error
+     (message "Error enabling copilot mode '%s', trying to install server" err)
+     (progn
+       (interactive)
+       (copilot-install-server)
+       )
+     )
+    )
+)
 
 (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
 (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
@@ -15,7 +27,8 @@
 (use-package copilot-chat
     :ensure t
     :straight (:host github :repo "chep/copilot-chat.el" :files ("*.el"))
-    :after (request org markdown-mode shell-maker))
+    :after (copilot request org markdown-mode shell-maker)
+)
 
 ;; I think popper makes this bothersome, so turn it off
 (setq copilot-indent-offset-warning-disable t)
--- personal/preload/theme.el
+++ personal/preload/theme.el
@@ -1,16 +1,19 @@
 ;; (setq prelude-theme 'sanityinc-tomorrow-eighties)
 ;; (setq prelude-theme 'solarized-dark)
 
-(use-package autothemer
-  :ensure t)
 (use-package dracula-theme
-  :ensure t)
-(use-package rose-pine-theme
-  :after autothemer
   :ensure t
-  :vc (:url "https://github.com/konrad1977/pinerose-emacs"
-            :rev :newest
-            :branch "main"))
+  :demand t
+  :straight (:host github :repo "dracula/emacs" :files ("*.el"))
+  :config
+  (setq prelude-theme 'dracula))
 
-(setq prelude-theme 'dracula)
+;; (use-package autothemer
+;;   :ensure t)
+;; (use-package rose-pine-theme
+;;   :after autothemer
+;;   :ensure t
+;;   :vc (:url "https://github.com/konrad1977/pinerose-emacs"
+;;             :rev :newest
+;;             :branch "main"))
 ;; (setq prelude-theme 'rose-pine)
