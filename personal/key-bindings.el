966c025e3c1fab86f71e64c6a259baa12a725a7c
Edd Wilder-James <edd@me.com>
2025-11-28 13:27:12 -0800

    set global binding for org-capture

--- personal/key-bindings.el
+++ personal/key-bindings.el
@@ -3,3 +3,6 @@
 (global-set-key (kbd "C-<end>") 'avy-goto-char-timer)
 (global-set-key (kbd "M-[") 'previous-buffer)
 (global-set-key (kbd "M-]") 'next-buffer)
+
+;; org mode
+(global-set-key (kbd "C-c c") 'org-capture)
