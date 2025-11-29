54d47e1e482fce5c1cb9ff45b0502101c3d4066f
Edd Wilder-James <edd@me.com>
2025-11-28 17:38:56 -0800

    sort out scrolling better

--- personal/scroll.el
+++ personal/scroll.el
@@ -1,26 +1,8 @@
-;; thanks to https://def.lakaban.net/2023-03-05-high-quality-scrolling-emacs/
-
-(defun filter-mwheel-always-coalesce (orig &rest args)
-  "A filter function suitable for :around advices that ensures only
-   coalesced scroll events reach the advised function."
-  (if mwheel-coalesce-scroll-events
-      (apply orig args)
-    (setq mwheel-coalesce-scroll-events t)))
-
-(defun filter-mwheel-never-coalesce (orig &rest args)
-  "A filter function suitable for :around advices that ensures only
-   non-coalesced scroll events reach the advised function."
-  (if mwheel-coalesce-scroll-events
-      (setq mwheel-coalesce-scroll-events nil)
-    (apply orig args)))
-
-;; Don't coalesce for high precision scrolling
-(advice-add 'pixel-scroll-precision :around #'filter-mwheel-never-coalesce)
-
-;; Coalesce for default scrolling (which is still used for horizontal scrolling)
-;; and text scaling (bound to ctrl + mouse wheel by default).
-(advice-add 'mwheel-scroll          :around #'filter-mwheel-always-coalesce)
-(advice-add 'mouse-wheel-text-scale :around #'filter-mwheel-always-coalesce)
-
-
-(pixel-scroll-mode 1)
+;; -*- lexical-binding: t; -*-
+(use-package ultra-scroll
+  :ensure t
+  :init
+  (setq scroll-conservatively 3 ; or whatever value you prefer, since v0.4
+        scroll-margin 0)        ; important: scroll-margin>0 not yet supported
+  :config
+  (ultra-scroll-mode 1))
