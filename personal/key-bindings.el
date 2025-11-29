c45ec4f9a71ab6f02989b0fbbc02fef02baed828
Edd Wilder-James <edd@me.com>
2025-11-28 21:14:43 -0800

    Fix rogue key binding breaking emacs in terminals

--- personal/key-bindings.el
+++ personal/key-bindings.el
@@ -1,8 +1,9 @@
 ;; -*- lexical-binding: t -*-
 ;; custom key bindings for my liking
 (global-set-key (kbd "C-<end>") 'avy-goto-char-timer)
-(global-set-key (kbd "M-[") 'previous-buffer)
-(global-set-key (kbd "M-]") 'next-buffer)
+(global-set-key (kbd "C-c [") 'previous-buffer)                                   │
+(global-set-key (kbd "C-c ]") 'next-buffer)                                       │
+
 
 ;; org mode
 (global-set-key (kbd "C-c c") 'org-capture)
