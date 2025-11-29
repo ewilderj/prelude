cf77be0765203054e8a2deed2f2257e389519483
Edd Wilder-James <edd@me.com>
2025-11-28 21:17:06 -0800

    fix pasto

--- personal/key-bindings.el
+++ personal/key-bindings.el
@@ -1,9 +1,8 @@
 ;; -*- lexical-binding: t -*-
 ;; custom key bindings for my liking
 (global-set-key (kbd "C-<end>") 'avy-goto-char-timer)
-(global-set-key (kbd "C-c [") 'previous-buffer)                                   │
-(global-set-key (kbd "C-c ]") 'next-buffer)                                       │
-
+(global-set-key (kbd "C-c [") 'previous-buffer)
+(global-set-key (kbd "C-c ]") 'next-buffer)
 
 ;; org mode
 (global-set-key (kbd "C-c c") 'org-capture)
