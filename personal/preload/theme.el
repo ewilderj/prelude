e4adb200268b51adb3e18604ce9a6426e57fee86
Edd Wilder-James <ewilderj@github.com>
2026-01-26 23:48:55 -0800

    Defer theme setup for daemon mode, disable prior themes

--- personal/preload/theme.el
+++ personal/preload/theme.el
@@ -26,15 +26,27 @@
 (add-to-list 'custom-theme-load-path prelude-personal-dir)
 
 ;; Use dracula-light in graphical mode, regular dracula in terminal
-(if (display-graphic-p)
-    (progn
-      (setq dracula-light-bg "#FAF9F5") ;; Tone down the creaminess
-      (setq dracula-light-comment "#6272A4") ;; Blue-grey comments
-      (setq prelude-theme 'dracula-light))
-  (use-package dracula-theme
-    :ensure t
-    :straight (:host github :repo "dracula/emacs" :files ("*.el")))
-  (setq prelude-theme 'dracula))
+;; Must be deferred for server mode (no frame exists at preload time)
+(defun ewilderj-setup-theme (&optional frame)
+  "Set theme based on display type. Works in both daemon and normal startup."
+  (when frame (select-frame frame))
+  ;; Disable any existing themes to prevent face inheritance issues
+  (mapc #'disable-theme custom-enabled-themes)
+  (if (display-graphic-p)
+      (progn
+        (setq dracula-light-bg "#FAF9F5") ;; Tone down the creaminess
+        (setq dracula-light-comment "#6272A4") ;; Blue-grey comments
+        (setq prelude-theme 'dracula-light)
+        (load-theme 'dracula-light t))
+    (use-package dracula-theme
+      :ensure t
+      :straight (:host github :repo "dracula/emacs" :files ("*.el")))
+    (setq prelude-theme 'dracula)
+    (load-theme 'dracula t)))
+
+(if (daemonp)
+    (add-hook 'after-make-frame-functions #'ewilderj-setup-theme)
+  (ewilderj-setup-theme))
 
 
 ;; (use-package dracula-light-theme
